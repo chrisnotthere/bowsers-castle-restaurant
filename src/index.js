6ae117aa30deaa21ed729b171fe8e3bbb2e08ab8
Chris <mr.chrisnash@gmail.com>
2021-07-28 11:31:32 -0700

    setting up basic website look

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-console.log('it is working!!!');
+import './style.css';
 
+console.log('this is a test');
+
+const content = document.querySelector('div[id="content"]');
+let element = document.createElement('div');
+//element.id = 'elem';
+
+element.innerHTML = 'hey this is aaaa test';
+element.classList.add('hello');
+
+content.appendChild(element);
