93e5369930bd5fb99116631690d44c2b604c2341
Chris <mr.chrisnash@gmail.com>
2021-07-30 14:53:43 -0700

    fixed bug when loading page for first time

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 import './style.css';
 import loadHome from './home.js';
 
-function initializeWebsite(){
-    const content = document.querySelector('div[id="content"]');
-    content.appendChild(loadHome());
-}
-
-initializeWebsite();
+loadHome();
